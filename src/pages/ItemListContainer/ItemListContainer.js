04fe917785774ddcb9e49d172b646304cdcef317
ezequiel <saavedraeze66@gmail.com>
2023-02-02 12:18:34 -0300

    filtrado Firebase

--- src/pages/ItemListContainer/ItemListContainer.js
+++ src/pages/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,11 @@
 import "./ItemListContainer.css";
 import Container from 'react-bootstrap/Container';
 import ItemList from "../../components/ItemList/ItemList";
-import ItemCategory from "../../components/ItemCategory/ItemCategory";
 import Row from 'react-bootstrap/Row';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loader from "../../components/Loader/Loader";
-import { collection, getDocs, getFirestore} from "firebase/firestore"
+import { collection, getDocs, getFirestore, where, query } from "firebase/firestore"
 
 function ItemListContainer() {
 
@@ -14,15 +13,13 @@ function ItemListContainer() {
     const [loading, setLoading] = useState(true);
     const { categoria } = useParams();
 
-    if(categoria) {
-
-    }
     const getProducts = () => {
         const dataBase = getFirestore();
-        const queryFirestore = collection(dataBase, "products")
-        getDocs(queryFirestore).then((response) => {  
-            const info = response.docs.map ((prod) => {
-                return {id: prod.id, ...prod.data()}
+        const queryBase = collection(dataBase, "products");
+        const querySnapshot = categoria ? query(queryBase, where("categoria", "==", categoria)) : queryBase;
+        getDocs(querySnapshot).then((response) => {
+            const info = response.docs.map((prod) => {
+                return { id: prod.id, ...prod.data() }
             });
             setLoading(false)
             setProducts(info)
@@ -30,37 +27,16 @@ function ItemListContainer() {
     }
 
     useEffect(() => {
-        getProducts()
-    }, [])
+        getProducts();
+    }, [categoria])
 
-    if (loading) {
-        return (
-            <Container fluid className="home">
-                <Row className="justify-content-center">
-                    <Loader />
-                </Row>
-            </Container>
-        )
-    } else {
-        if (categoria) {
-            return (
-                <Container fluid className="home">
-                    <Row className="justify-content-center">
-                        <ItemCategory listaProductos={productos} />
-                    </Row>
-                </Container>
-            )
-        } else {
-            return (
-                <Container fluid className="home">
-                    <Row className="justify-content-center">
-                        <ItemList listaProductos={productos} />
-                    </Row>
-                </Container>
-            )
-        }
-    }
+    return (
+        <Container fluid className="home">
+            <Row className="justify-content-center">
+                {loading ? <Loader /> : <ItemList listaProductos={productos} />}
+            </Row>
+        </Container>
+    )
 }
-
 
 export default ItemListContainer;
